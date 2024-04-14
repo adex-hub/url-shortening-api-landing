d439f524014c4d44e3d8b4f469a6725de1f9e63b
adex-hub <adeolaweb@gmail.com>
2024-04-15 00:04:56 +0100

    update: more semantic improvements

--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -5,9 +5,9 @@ export default function Stats() {
     <section className="justify-center z-0">
       {/* TEXT SECTION */}
       <div className="text-center space-y-2 flex flex-col items-center">
-        <h3 className="text-verydarkviolet mt-20 text-[27px] lg:text-[2.5rem] font-bold">
+        <h2 className="text-verydarkviolet mt-20 text-[27px] lg:text-[2.5rem] font-bold">
           Advanced Statistics
-        </h3>
+        </h2>
         <p className="text-grayishviolet text-[18px] w-[90%] lg:w-[38%] mx-auto font-[500] mb-[32px] leading-7">
           Track how your links are performing across the web with our advanced
           statistics dashboard.
@@ -17,5 +17,3 @@ export default function Stats() {
     </section>
   );
 }
-
-// REMEMBER: You changed thing from padding at the section class level to margin at the h3 level
